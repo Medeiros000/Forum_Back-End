b70a95eebc073f2536c5614f276af051754663d1
Medeiros000 <98979459+Medeiros000@users.noreply.github.com>
2023-08-29 22:37:35 -0300

    Rollback Add Cors Mappings

--- Forum/src/main/java/com/br/alura/forum/ForumApplication.java
+++ Forum/src/main/java/com/br/alura/forum/ForumApplication.java
@@ -2,9 +2,6 @@ package com.br.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ForumApplication {
@@ -12,16 +9,5 @@ public class ForumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/api/**")
-						.allowedOrigins("*") // Permitir solicitações de qualquer origem
-						.allowedMethods("GET", "POST", "PUT", "DELETE")
-						.allowedHeaders("*");
-			}
-		};
-	}
+
 }
