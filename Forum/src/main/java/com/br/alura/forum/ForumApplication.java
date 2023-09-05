093e0e8294a7d8cfe2fbbe1eb518ac615edf333b
Medeiros000 <98979459+Medeiros000@users.noreply.github.com>
2023-09-05 14:51:38 -0300

    Cors Configurado

--- Forum/src/main/java/com/br/alura/forum/ForumApplication.java
+++ Forum/src/main/java/com/br/alura/forum/ForumApplication.java
@@ -2,9 +2,6 @@ package com.br.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ForumApplication {
@@ -12,15 +9,5 @@ public class ForumApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
 	}
-	// Configurando o Access-Control-Allow-Origin
-	 @Bean
-	 public WebMvcConfigurer corsConfigurer() {
-	     return new WebMvcConfigurer() {
-	         @Override
-	         public void addCorsMappings(CorsRegistry registry) {
-	             registry.addMapping("/**").allowedOrigins("*");
-	         }
-	     };
-	 }
 
 }
--- Forum/src/main/java/com/br/alura/forum/infra/security/SecurityConfigurations.java
+++ Forum/src/main/java/com/br/alura/forum/infra/security/SecurityConfigurations.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,7 +14,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
@@ -32,6 +32,8 @@ public class SecurityConfigurations {
                         .requestMatchers(HttpMethod.POST, "/usuarios").hasAnyRole("*")
                         .anyRequest().authenticated()
                 );
+        http.cors(Customizer.withDefaults());
+
         http.addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
